b8074317f5d3fa9552ef2e36a203812dab7a67cb
manas-shinde <manas.a.shinde11@gmail.com>
2023-07-29 18:36:31 +0530

    mapping updated.

--- dist/03 - Advanced Types/src/index.js
+++ dist/03 - Advanced Types/src/index.js
@@ -15,4 +15,31 @@ function kgToLbs(weight) {
 }
 console.info(`10 kg = ${kgToLbs(10)} lbs`);
 console.info(`50 kg = ${kgToLbs('50kg')} lbs`);
+let testBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let limit = 50;
+function greet(name) {
+    if (name)
+        console.log(`Greetings! ${name.toUpperCase()} `);
+    else
+        console.log('Hola!');
+}
+greet(null);
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30
+};
+const user = [
+    {
+        name: "manas",
+        age: 24,
+        occupation: 'SWE'
+    },
+    {
+        name: 'Akshay Patil',
+        age: 23
+    }
+];
 //# sourceMappingURL=index.js.map
