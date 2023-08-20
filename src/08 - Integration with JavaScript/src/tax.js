e1c5ea56bff4c981b900a722f265b90cbd790b99
manas-shinde <manas.a.shinde11@gmail.com>
2023-08-20 18:19:48 +0530

    Added comments for JSDocs.

--- src/08 - Integration with JavaScript/src/tax.js
+++ src/08 - Integration with JavaScript/src/tax.js
@@ -1,3 +1,18 @@
+// /* Describing Types Using JSDoc */
+//
+// /**
+//  * Calculates income tax.
+//  * @param {number} income - Net salary after expenses
+//  * @returns {number}
+//  */
+//
+// export function calculateTax(income) {
+//     return income * .3;
+// }
+
+/**
+ * @param {number} income
+ */
 export function calculateTax(income) {
   return income * 0.3;
 }
